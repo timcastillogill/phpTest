9720a416a4d7b4ee184bfd96cf44ef28470cec53
Emily Worrall <emily.worrall@futurelearn.com>
2021-07-01 18:07:37 +0100

    Add function to display greeting and date

--- test.php
+++ test.php
@@ -19,9 +19,24 @@ echo $txt;
 define("HOUSE", "Surbiton");
 echo HOUSE,  $color, "My favourite color";
 echo "<h3 style='color: blue; text-align: center'>This is my favourite color</h3>";
-echo "<h3 " . $style . "'text-align: center'>This is my favourite other color</h3>";
+echo "<h3 $style 'text-align: center'>This is my favourite other color</h3>";
 print "Hello Blue board!";
 
 echo strlen(HOUSE);
+
+?>
+</p>
+
+<p>
+<?php
+function whatIsToday($greetings=["my default greeting"]){
+  foreach($greetings as $blahblah){
+    echo $blahblah . "Today is " . date('D', mktime());
+  }
+}
+
+$thisIsMyGreeting = ["Good morning!", "what's up?"];
+whatIsToday();
+
 ?>
 </p>
